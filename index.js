09ae388670eb1b337f71f4c9b7c48c9a332b9820
ddot727 <ddot727@gmail.com>
2019-03-18 16:57:53 -0400

    set server up

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Whats good? Here come that boi!')
+});
+
+server.listen(4000, () => {
+  console.log('\n*** Running on port 4k ***\n')
+})
